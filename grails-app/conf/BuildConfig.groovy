74848845190cbc60708919e4492466e9be4100aa
Goran Ehrsson <goran@technipelago.se>
2017-01-21 20:21:05 +0100

    Upgrade plugins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -45,11 +45,11 @@ grails.project.dependency.resolution = {
             export = false
         }
 
-        test(":codenarc:0.25.2") { export = false }
+        test(":codenarc:0.26.0") { export = false }
         test(":code-coverage:2.0.3-3") { export = false }
 
         compile ":crm-feature:2.4.3"
-        compile ":crm-security:2.4.3"
+        compile ":crm-security:2.4.4"
         compile ":crm-contact:2.4.2"
         compile ":crm-tags:2.4.2"
 
